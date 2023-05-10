578cf4862de8c79d2e39d0e3bbce6de906798ace
Yin Lee <leesrrh@gmail.com>
2023-05-10 16:32:27 +0900

    completed

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: { in: CATEGORY }
+end
